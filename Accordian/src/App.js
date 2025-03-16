1a5995f323a0e338cb4d942545ed2920d7b66068
Deepak Chyaunal <52254018+Dee-blip@users.noreply.github.com>
2025-03-16 14:51:29 +0530

    Add files via upload

---
+++ Accordian/src/App.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+import Faq from "./Components/Faq";
+
+export default function App() {
+  return <Faq />;
+}
---
+++ Accordian/src/Components/Faq.jsx
@@ -0,0 +1,14 @@
+import Accordion from "./Accordion";
+import data from "../data.json";
+
+export default function Faq() {
+  // console.log(data);
+  return (
+    <div>
+      <h1> FAQ's </h1>
+      {data.faqs.map((obj, index) => {
+        return <Accordion key={index} qna={obj} />;
+      })}
+    </div>
+  );
+}
